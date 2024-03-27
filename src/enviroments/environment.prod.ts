e855a40d613c8c1b2892dabcf140e42856dc846c
EuniceLuaLuoShan <eunicelualuoshan@gmail.com>
2024-03-27 18:03:58 +0800

    setup environment

---
+++ src/enviroments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  enableMsal: false,
+  baseApiServiceUrl: 'http://localhost:3000/api',
+  baseAccessScopeUrl: '',
+  currentUrl: 'http://localhost:4200',
+  azureAppConfigConn: '',
+  aadClientId: '',
+  aadAuthority: 'https://login.microsoftonline.com/common',
+  apiKey: ''
+};
---
+++ src/enviroments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  enableMsal: true,
+  baseApiServiceUrl: '#{{baseApiServiceUrl}}#',
+  baseAccessScopeUrl: '#{{baseAccessScopeUrl}}#',
+  currentUrl: '#{{currentUrl}}#',
+  azureAppConfigConn: '',
+  aadClientId: '#{{aadClientId}}#',
+  aadAuthority: '#{{aadAuthority}}#',
+  apiKey: '#{{apiKey}}#'
+};
